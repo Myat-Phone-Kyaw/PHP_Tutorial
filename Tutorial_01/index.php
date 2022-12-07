59b8a76f7a1f2da81b8fc99a13f0293d26c389d5
Myat Phone Kyaw <max@Myats-MacBook-Air.local>
2022-12-07 14:11:41 +0630

    tuto1

---
+++ Tutorial_01/index.php
@@ -0,0 +1,41 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial 1 (Chess)</title>
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<table>
+    <?php
+    $col = 0;
+
+    while ($col < 8) 
+    {
+        $row = 0;
+        echo "<tr>";
+        $value = $col;
+
+        while ($row < 8)
+        {
+            if ($value % 2 == 0)
+            {
+                echo '<td class="white"></td>';
+                $value++;
+            }
+            else 
+            {
+                echo '<td class="black"></td>';
+                $value++;
+            }
+            $row++;
+        }
+        echo "</tr>";
+        $col++;
+    }
+    ?>
+    </table>
+</body>
+</html>
