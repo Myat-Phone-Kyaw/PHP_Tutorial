857e2970788e056ce9d4a071e13987f7e115c6cd
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2022-12-08 15:15:31 +0630

    update

--- Tutorial_02/index.php
+++ Tutorial_02/index.php
@@ -1,50 +1,47 @@
 <?php
-  function diamond($n)
-  {
-  $space = 6;
-  $i = 0;
-  while($i < $n)
-  {
-    $j = 0;
-    while ($j < $space) {
-      echo " &nbsp;";
-      $j++;
+function diamond($n)
+{
+    $space = 6;
+    $i = 0;
+    while ($i < $n) {
+        $j = 0;
+        while ($j < $space) {
+            echo " &nbsp;";
+            $j++;
+        }
+
+        $j = 0;
+        while ($j <= $i) {
+            echo "*";
+            $j++;
+        }
+
+        echo "<br>";
+        $space--;
+        $i += 2;
     }
 
-    $j = 0;
-    while($j <= $i)
-    {
-      echo "*";
-      $j++;
+    //Reverse
+    $space = 2;
+    $i = $n - 2;
+    while ($i > 0) {
+        $j = 0;
+        while ($j < $space) {
+            echo " &nbsp;";
+            $j++;
+        }
+
+        $j = 0;
+        while ($j < $i) {
+            echo "*";
+            $j++;
+        }
+
+        echo "<br>";
+        $space++;
+
+        $i -= 2;
     }
-
-    echo "<br>";
-    $space--;
-    $i+=2;
-  }
-
-  $space = 2;
-  $i = $n-2;
-  while($i > 0)
-  {
-    
-    $j = 0;
-    while($j < $space){
-      echo " &nbsp;";
-      $j++;
-    }
-
-    $j = 0;
-    while($j < $i){
-      echo "*";
-      $j++;
-    }
-
-    echo "<br>";
-    $space++;
-
-    $i-=2;
-  }
 }
 
 diamond(11);
