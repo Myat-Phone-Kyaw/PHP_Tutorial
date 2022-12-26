0a8a611aa49eb2aaea68daf11003443b8d873169
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2022-12-26 14:01:13 +0630

    Review fix

--- Tutorial_07/index.php
+++ Tutorial_07/index.php
@@ -1,19 +1,24 @@
 <?php
 require_once './lib/phpqrcode/qrlib.php';
 
-$path = 'images/';
-if (!is_dir($path)) {
-    mkdir($path);
-}
+    $validMsg = "";
+    $path = 'images/';
+    if (!is_dir($path)) {
+        mkdir($path);
+    }
 
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $file = $path.'/'.$name.'.png';
+    if (isset($_POST['submit'])) {
+        if (empty($_POST['name'])) {
+            $validMsg = 'Input field is required to generate QRcode.';
+        } else {
+            $name = $_POST['name'];
+        $file = $path.'/'.$name.'.png';
 
-    QRcode::png($name, $file, 'H', 8, 4);
+        QRcode::png($name, $file, 'H', 8, 4);
 
-    echo '<img src="'.$file.'" class="qr">';
-}
+        echo '<img src="'.$file.'" class="qr">';
+        }
+    }
 ?>
 
 <link rel="stylesheet" href="css/reset.css">
@@ -24,7 +29,9 @@ if(isset($_POST['submit'])){
         <div class="form-div">
             <label class="label">Enter the data: </label>
             <input class="input" type="text" name="name" placeholder="File Name">
+            <p class="warning"><?php echo $validMsg; ?></p>
         </div>
+        
         <div class="div-center">
         <button type="submit" name="submit" class="upload">Upload</button>
         </div>
