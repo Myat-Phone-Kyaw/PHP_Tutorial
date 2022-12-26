40794b79b8cd23e456971ab9ef172d4cc30a7ca2
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2022-12-26 13:48:06 +0630

    Review fix

--- Tutorial_05/index.php
+++ Tutorial_05/index.php
@@ -13,65 +13,65 @@
 
 <body>
     <?php
-    $myfile = fopen("file/sample.txt", "r") or die("Unable to open file!");
-    $txt = fread($myfile, filesize("file/sample.txt"));
-    fclose($myfile);
+        $myfile = fopen("file/sample.txt", "r") or die("Unable to open file!");
+        $txt = fread($myfile, filesize("file/sample.txt"));
+        fclose($myfile);
 
-    require './lib/vendor/autoload.php';
-    $content = '';
-    $reader = \PhpOffice\PhpWord\IOFactory::createReader("Word2007");
-    $phpWord = $reader->load("file/sample.docx");
-    foreach ($phpWord->getSections() as $section) {
-        foreach ($section->getElements() as $element) {
-            if (method_exists($element, 'getElements')) {
-                foreach ($element->getElements() as $childElement) {
-                    if (method_exists($childElement, 'getText')) {
-                        $content .= $childElement->getText() . ' ';
-                    } else if (method_exists($childElement, 'getContent')) {
-                        $content .= $childElement->getContent() . ' ';
+        require './lib/vendor/autoload.php';
+        $content = '';
+        $reader = \PhpOffice\PhpWord\IOFactory::createReader("Word2007");
+        $phpWord = $reader->load("file/sample.docx");
+        foreach ($phpWord->getSections() as $section) {
+            foreach ($section->getElements() as $element) {
+                if (method_exists($element, 'getElements')) {
+                    foreach ($element->getElements() as $childElement) {
+                        if (method_exists($childElement, 'getText')) {
+                            $content .= $childElement->getText() . ' ';
+                        } else if (method_exists($childElement, 'getContent')) {
+                            $content .= $childElement->getContent() . ' ';
+                        }
                     }
+                } else if (method_exists($element, 'getText')) {
+                    $content .= $element->getText() . ' ';
                 }
-            } else if (method_exists($element, 'getText')) {
-                $content .= $element->getText() . ' ';
             }
         }
-    }
 
-    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-    $xlsx = $reader->load("file/sample.xlsx");
-    $excel_data = $xlsx->getActiveSheet()->toArray();
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        $xlsx = $reader->load("file/sample.xlsx");
+        $excel_data = $xlsx->getActiveSheet()->toArray();
 
-    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
-    $csv = $reader->load("file/sample.csv");
-    $csv_data = $csv->getActiveSheet()->toArray();
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        $csv = $reader->load("file/sample.csv");
+        $csv_data = $csv->getActiveSheet()->toArray();
     ?>
     <div class="container">
-        <h2>Text file .txt</h2>
+        <h2 class="title">Text file .txt</h2>
         <p>
             <?php echo $txt; ?>
         </p>
-        <h2>Document word file .doc</h2>
+        <h2 class="title">Document word file .doc</h2>
         <p>
             <?php echo $content; ?>
         </p>
-        <h2>Excel file .xlsx</h2>
+        <h2 class="title">Excel file .xlsx</h2>
         <table>
             <?php foreach ($excel_data as $excel) { ?>
             <tr>
                 <?php foreach ($excel as $key => $value) { ?>
                 <td>
                     <?php echo $value; ?>
-                </td>
+                </td class="data">
                 <?php } ?>
             </tr>
             <?php } ?>
         </table>
-        <h2>.csv</h2>
+        <h2 class="title">.csv</h2>
         <table>
             <?php foreach ($csv_data as $comma) { ?>
             <tr>
                 <?php foreach ($comma as $key => $value) { ?>
-                <td>
+                <td class="data">
                     <?php echo $value; ?>
                 </td>
                 <?php } ?>
